6395be14cdf11fdb33f5515cccf432b493295fba
Zachary Nelson <nelsonz2013@hotmail.com>
2020-04-14 18:16:28 -0500

    remote link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
     return(
         <Container fluid className='footer text-center mt-4'>
-            <div><a href='mailto:zachatrework@gmail.com'>Contact Us</a></div>
+            <div><a href='#'>Contact Us</a></div>
             <span>Built with &#10084; in Austin Texas</span>
         </Container>
     );
